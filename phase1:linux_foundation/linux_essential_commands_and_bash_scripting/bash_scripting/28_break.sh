9cfa037ebd381e0b8c092eb5ad96600167e0259a
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-09-04 10:12:10 +0530

    break

---
+++ phase1:linux_foundation/linux_essential_commands_and_bash_scripting/bash_scripting/28_break.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#Example of break in a loop
+#We just need to confirm if a give no. if present or not
+
+no=6
+
+for i in {1..9}
+do
+	#Break the loop if no. found
+	if [[ $no -eq $i ]]
+	then
+		echo "$no is found!!!!"
+		break
+	fi
+	echo "Number is $i"
+done
