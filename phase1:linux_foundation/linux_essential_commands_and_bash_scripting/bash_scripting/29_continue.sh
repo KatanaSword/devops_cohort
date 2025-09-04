a2f8852032abcb49d41bea0ae7bb29ffd68f8868
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-09-04 10:12:42 +0530

    continue

---
+++ phase1:linux_foundation/linux_essential_commands_and_bash_scripting/bash_scripting/29_continue.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Example of using continue in loop
+#Suppose we only need to print odd no.
+
+for i in {1..10}
+do
+	let r=$i%2
+	if [[ $r -eq 0 ]]
+	then
+		continue
+	fi
+	echo "Odd no. is: $i"
+done
